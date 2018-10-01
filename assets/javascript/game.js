6ccd7df29c11de5e047c296cb257f1cbce40568e
Ordolph <austin.t.randolph@gmail.com>
2018-09-30 20:32:59 -0400

    started on js, added array for word choices and variable that picks one at random from array

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var nintendoArray = [
+    "Gameboy" , "Gamecube", "Dual Screen", "WiiU", "Switch",
+     "Pokemon", "Metroid", "Mario Bros.", "Castlevania",
+      "Smash Bros.", "Mario Kart", "Mario Party", "Pikachu",
+       "Mario", "Luigi", "Link", "Zelda", "Gannondorf", 
+       "Samus", "Earthbound", "Captain Falcon", "F Zero",
+        "Star Fox", "Falco", "Peach", "Daisy", "Wario"
+    ];
+    
+    var wordGuess = nintendoArray[Math.floor(Math.random() * nintendoArray.length)]
+
+    console.log (wordGuess)
+
+    
